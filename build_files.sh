8cfdd813f36c98e371f77369f1daae01c2be2ff6
Patni Chirag <PATNI-CHIRAG>
2025-08-26 13:44:28 +0530

    Fix: Specify python version for pip in build script

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # build_files.sh
-pip install -r requirements.txt
+
+# Install dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
+
+# Apply database migrations
 python3.9 manage.py migrate
